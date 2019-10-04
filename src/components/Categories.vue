eae8b598e129b7571c0bad4584710416f47ee219
root <root@localhost.localdomain>
2019-10-04 11:46:21 +0530

    buid again for testing

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -4,8 +4,13 @@
             <v-content class="mx-2" >
                 <h2>Browse by Categories</h2>
             </v-content>
+
+            <div>
+                <v-row>
+                    
+                </v-row>
+            </div>
         </div>
-        
     </div>
 </template>
 
