344273a4c44bf940ae5ce9e0df23057c03e264e2
JordanSaurav <janardhansingh1998@gmail.com>
2019-10-24 22:26:12 +0530

    some modification in pwa 2

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
       workboxPluginMode: 'InjectManifest',
       workboxOptions: {
         // swSrc is required in InjectManifest mode.
-        swSrc: 'dev/sw.js',
+        swSrc: 'public/manifest.json',
         // ...other Workbox options...
       }
     }
