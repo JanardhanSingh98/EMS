a1a0cb417ac575786c5aca0d4a83f68a26b85ed0
JordanSaurav <janardhansingh1998@gmail.com>
2019-10-26 04:02:50 +0530

    working on pwa 3

--- vue.config.js
+++ vue.config.js
@@ -1,49 +1,6 @@
-/*module.exports = {
-    // ...other vue-cli plugin options...
-    pwa: {
-      
-      // configure the workbox plugin
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        // swSrc is required in InjectManifest mode.
-        swSrc: 'src/registerServiceWorker.js',
-        // ...other Workbox options...
-      }
-    }
-}*/
-
-/*import manifestJSON from "./public/manifest.json";
-pwa = {
-  themeColor: manifestJSON.theme_color,
-  name: manifestJSON.short_name,
-  msTileColor: manifestJSON.background_color
-};*/
 
 
-/*module.export = {
-  productionSourceMap: false,
-  pwa: {
-      name: ,
-      themeColor: '#3F51B5',
-      appleMobileWebAppStatusBarStyle: '#000000',
-  }
-}*/
-
-/*export const pwa = {
-  name: 'My App',
-  themeColor: '#ffffff',
-  msTileColor: '#ffffff',
-  appleMobileWebAppCapable: 'yes',
-  appleMobileWebAppStatusBarStyle: 'black',
-  // configure the workbox plugin
-  workboxPluginMode: 'InjectManifest',
-  workboxOptions: {
-    // swSrc is required in InjectManifest mode.
-    swSrc: 'src/registerServiceWorker.js',
-  }
-};*/
-
-const manifestJSON = require('./public/manifest.json')
+/*const manifestJSON = require('./public/manifest.json')
 module.exports = {
   pwa: {
     themeColor: manifestJSON.theme_color,
@@ -56,4 +13,4 @@ module.exports = {
       swSrc: 'service-worker.js',
     },
   },
-}
+}*/
