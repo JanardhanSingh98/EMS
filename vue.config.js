5e50f819ac3c673b883d8ab32f125cc07278e4bb
JordanSaurav <janardhansingh1998@gmail.com>
2019-10-26 02:29:41 +0530

    pwa update feature added

--- vue.config.js
+++ vue.config.js
@@ -11,3 +11,49 @@
       }
     }
 }*/
+
+/*import manifestJSON from "./public/manifest.json";
+pwa = {
+  themeColor: manifestJSON.theme_color,
+  name: manifestJSON.short_name,
+  msTileColor: manifestJSON.background_color
+};*/
+
+
+/*module.export = {
+  productionSourceMap: false,
+  pwa: {
+      name: ,
+      themeColor: '#3F51B5',
+      appleMobileWebAppStatusBarStyle: '#000000',
+  }
+}*/
+
+/*export const pwa = {
+  name: 'My App',
+  themeColor: '#ffffff',
+  msTileColor: '#ffffff',
+  appleMobileWebAppCapable: 'yes',
+  appleMobileWebAppStatusBarStyle: 'black',
+  // configure the workbox plugin
+  workboxPluginMode: 'InjectManifest',
+  workboxOptions: {
+    // swSrc is required in InjectManifest mode.
+    swSrc: 'src/registerServiceWorker.js',
+  }
+};*/
+
+const manifestJSON = require('./public/manifest.json')
+module.exports = {
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+    },
+  },
+}
