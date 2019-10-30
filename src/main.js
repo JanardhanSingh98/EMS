d68e2b700537cf7411cb3f591ced9d60b157a708
JordanSaurav <janardhansingh1998@gmail.com>
2019-10-31 04:05:25 +0530

    removed unwanted plugins

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueFilterDateFormat from 'vue-filter-date-format';
 import store from './store'
 
 Vue.use(VueAxios, axios)
@@ -17,22 +16,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-
-Vue.use(VueFilterDateFormat, {
-  dayOfWeekNames: [
-    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-    'Friday', 'Saturday'
-  ],
-  dayOfWeekNamesNamesShort: [
-    'Su', 'Mo', 'Tu', 'We', 'Tr', 'Fr', 'Sa'
-  ],
-  monthNames: [
-    'January', 'February', 'March', 'April', 'May', 'June',
-    'July', 'August', 'September', 'October', 'November', 'December'
-  ],
-  monthNamesShort: [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-  ]
-});
-
